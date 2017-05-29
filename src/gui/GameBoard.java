6474c44617ec097884c680b9312127664b85d61d
Sara Bracha <stanenba2@student.touro.edu>
2017-05-28 20:22:49 -0400

    In the process of switching to vertical layout

--- src/gui/GameBoard.java
+++ src/gui/GameBoard.java
@@ -6,6 +6,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
@@ -39,38 +40,62 @@ public class GameBoard extends JFrame
 		mainFrame = new JPanel();
 		
 		mainFrame.setLayout(null);
+		mainFrame.setBackground(Color.WHITE);
 		//setBackground(Color.___ or Theme.___- make a theme so user can have options)
 		setContentPane(mainFrame);
+
+		gameTitle = new JLabel("Racko");
+		gameTitle.setLocation(575, 0);
+		gameTitle.setSize(1000,100);
+		gameTitle.setFont(new Font("Britannic Bold", Font.PLAIN, 56));
+		mainFrame.add(gameTitle);
 		user1Cards = new JButton[10];
 		
-
+		
 		
 		
 		
 		setUpBoards();
+		setUpPiles();
 		mainFrame.setVisible(true);
 	}
 	
 	private void setUpBoards()
 	{
-		ImageIcon background = createImageIcon("/cardFront.png");
+		ImageIcon background = createImageIcon("/partialCardFront.PNG");
 			
-		for(int card = 0; card < user1Cards.length; card++)
+		for(int card = user1Cards.length-1; card > 0 ; card--)
 		{
 			user1Cards[card] = new JButton(background);
+			user1Cards[card].setBorder(null);
 			//user1Cards[card].setText(deck.drawCard());
+
 			user1Cards[card].setHorizontalTextPosition(JButton.CENTER);
 			user1Cards[card].setVerticalTextPosition(JButton.CENTER);
-			user1Cards[card].setSize(90, 70);
-			user1Cards[card].setLocation(175 + (card*100), 600);
+			user1Cards[card].setSize(170, 130);
+			user1Cards[card].setLocation(40, 20 + (card*55));
 			mainFrame.add(user1Cards[card]);
-
+			//setComponentZOrder(user1Cards[card], card);
 		}
 	
+		computerBoard = new JLabel(createImageIcon("/ComputerBoard.png"));
+		computerBoard.setSize(700, 500);
+		computerBoard.setLocation(800, 150);
+		mainFrame.add(computerBoard);
 
 	}
 	
-
+private void setUpPiles()
+{
+	drawPile = new JButton(createImageIcon("/RackoCardBack.PNG"));
+	drawPile.setText("Draw Card");
+	drawPile.setFont(new Font("Britannic Bold", Font.PLAIN, 20));
+	drawPile.setHorizontalTextPosition(JButton.CENTER);
+	drawPile.setVerticalTextPosition(JButton.CENTER);
+	drawPile.setLocation(575, 350);
+	drawPile.setSize(220,170);
+	mainFrame.add(drawPile);
+}
 
 
 	/** Returns an ImageIcon, or null if the path was invalid. */
@@ -91,6 +116,8 @@ public class GameBoard extends JFrame
 	{
 		GameBoard frame = new GameBoard();
 		frame.setVisible(true);
+		
+		//Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
 	}
 }
